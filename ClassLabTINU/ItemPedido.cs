9c81efa9a2f8ec567d12b05ea38ec1753e3be730
Vinicius Maximo <vinimaximo033@hotmail.com>
2022-04-08 10:08:12 -0300

    list

--- ClassLabTINU/ItemPedido.cs
+++ ClassLabTINU/ItemPedido.cs
@@ -8,5 +8,52 @@ namespace ClassLabTINU
 {
     internal class ItemPedido
     {
+        //iped_ip idprod_ip valor quantidade desconto
+        //Atributos 
+        private Produto produto;
+        private double valor;
+        private double quantidade;
+        private double desconto;
+    
+        //Propriedades
+        public Produto Produto { get { return produto; } set { produto = value; } } 
+        public double Valor { get { return valor; } set { valor = value; } }
+        public double Quantidade { get { return quantidade; } set { quantidade = value; } }
+        public double Desconto { get { return desconto; } set { desconto = value; } }
+
+        //Construtores
+        public ItemPedido() { }
+
+        public ItemPedido(Produto produto, double valor, double quantidade, double desconto)
+        {
+            this.produto = produto;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.desconto = desconto;
+        }
+    
+    
+        //Métodos da classe - Ação da classe
+        public void Inserir() { }
+        public void Alterar(ItemPedido item)
+        {
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
+
 }
--- ClassLabTINU/Pedido.cs
+++ ClassLabTINU/Pedido.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ClassLabTINU
 {
     public class Pedido
     {
+        // idped data status_ped desconto idcli_ped iduser_ped
+        // Atributos
+
+        // Propriedades
+        public int Id { get; set; }
+        public DateTime DataPed { get; set; }
+        public string Status { get; set; }
+        public double Desconto { get; set; }
+        public Cliente Cliente { get; set; }
+        public Usuario Usuario { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+
+        // Construtores 
+        public Pedido() { }
+
+         
+        public Pedido (int id, DateTime dataPed, string status, double desconto, Cliente cliente, Usuario usuario, List<ItemPedido> itens)
+        {    
+            Id = id;
+            DataPed = dataPed;
+            Status = status;
+            Desconto = desconto;
+            Cliente = cliente;
+            Usuario = usuario;
+            Itens = itens;
+   
+ 
+        }
+        // Métodos da classe - Operações/Ações/Funções
+        public void Inserir() { }
+        public bool Alterar(Pedido pedido)
+        {
+            return false;
+        }
+        public static List<Pedido> ConsultarClienteId(int _id)
+        {
+            List<Pedido> pedidos = new List<Pedido>();
+            //Graça...conecta ao banco e realiza a consulta pelo id do cliente
+            return  pedidos;
+        }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }
